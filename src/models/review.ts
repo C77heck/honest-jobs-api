b89ef621c8f78a0f859081f45439fd7e8717cf2b
Zoltan Csilleri <zcsilleri@gmail.com>
2022-04-08 23:53:31 +0200

    wip

---
+++ src/models/review.ts
@@ -0,0 +1,66 @@
+import Mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+const Schema = mongoose.Schema;
+
+export interface ReviewDocument extends Document {
+    title: string;
+    description: string;
+    salary: string;
+    location: string;
+    roleHeld: string;
+    suggestions?: string;
+    rating: number;
+    pros: string[];
+    cons: string[];
+    startDate: Date;
+    finishDate: Date;
+    isCurrentEmployer: boolean;
+    employerId: mongoose.Types.ObjectId;
+}
+
+const reviewSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    salary: { type: String, required: true },
+    currency: { type: String, required: true },
+    location: { type: String, required: true },
+    roleHeld: { type: String, required: true },
+    rating: { type: Number, required: true },
+    suggestions: { type: String },
+    pros: [{ type: String, required: true }],
+    cons: [{ type: String, required: true }],
+    startDate: { type: Date, required: true },
+    finishDate: { type: Date },
+    isCurrentEmployer: { type: Boolean, required: true },
+    employerId: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
+});
+
+reviewSchema.set('timestamps', true);
+
+reviewSchema.plugin(uniqueValidator);
+
+interface AdModel extends Mongoose.Model<any> {
+    getReviewsForEmployer(this: Mongoose.Model<any>, employerId: string | number): Promise<ReviewDocument[]>;
+
+    updateReview(this: Mongoose.Model<any>, employerId: string | number, reviewData: ReviewDocument): Promise<ReviewDocument>;
+
+    deleteReview(this: Mongoose.Model<any>, employerId: string | number): Promise<any>;
+
+    getReviewById(this: Mongoose.Model<any>, employerId: string | number): Promise<ReviewDocument>;
+}
+
+reviewSchema.static('updateAd', async function (this: Mongoose.Model<any>, employerId: string | number, adData: ReviewDocument): Promise<ReviewDocument> {
+    return await this.updateOne({ _id: employerId }, adData);
+});
+
+reviewSchema.static('deleteAd', async function (this: Mongoose.Model<any>, employerId: string | number, adData: ReviewDocument): Promise<ReviewDocument> {
+    return await this.deleteOne({ _id: employerId });
+});
+
+reviewSchema.static('getById', async function (this: Mongoose.Model<any>, employerId: string | number, adData: ReviewDocument): Promise<ReviewDocument> {
+    return await this.findOne({ _id: employerId });
+});
+
+export default mongoose.model<ReviewDocument, AdModel>('Review', reviewSchema);
