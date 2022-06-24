5340d5a11a29e563a888e3592915802804c7ff31
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-06-24 09:24:09 +0200

    wip

---
+++ src/controllers/recruiter.controller.ts
@@ -0,0 +1,47 @@
+import Ad, { AdDocument } from '@models/ad';
+import { HttpError } from '@models/libs/http-error';
+import { NextFunction } from 'express';
+import { ERROR_MESSAGES } from '../libs/constants';
+
+export const createNewAd = async (req: any, res: any, next: NextFunction) => {
+    const createdAd: any = new Ad(req.body as AdDocument);
+
+    try {
+        await createdAd.save();
+    } catch (err) {
+        return next(new HttpError(
+            'Could not create Ad, please try again.',
+            500
+        ));
+    }
+
+    res.status(201).json({ message: 'New ad has been successfully added' });
+};
+
+export const updateAd = async (req: any, res: any, next: NextFunction) => {
+    let updatedAd: AdDocument;
+
+    try {
+        updatedAd = await Ad.updateAd(req.params.adId, req.body as AdDocument);
+    } catch (err) {
+        return next(new HttpError(
+            ERROR_MESSAGES.GENERIC,
+            500
+        ));
+    }
+
+    res.status(200).json({ updatedAd, message: 'Successfully updated.' });
+};
+
+export const deleteAd = async (req: any, res: any, next: NextFunction) => {
+    try {
+        await Ad.deleteAd(req.params.adId);
+    } catch (err) {
+        return next(new HttpError(
+            'Could not create Ad, please try again.',
+            500
+        ));
+    }
+
+    res.status(201).json({ message: 'New ad has been successfully added' });
+};
