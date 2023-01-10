cc1d3222fa92057ae9be94eb14185b1a00a1ae78
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2023-01-10 08:53:28 +0100

    wip

--- src/application/libs/singleton.ts
+++ src/application/libs/singleton.ts
@@ -1,9 +1,11 @@
 export class Singleton {
+    // private static _instance: Singleton = new Singleton();
     protected constructor() {
         //
     }
 
     public static get instance() {
+        // todo return the static _instance.
         return new this();
     }
 }
